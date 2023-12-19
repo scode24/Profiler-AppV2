163098a90d4e8002fa4131322a75a65789a1159d
Soumyabrata Sarkar <smy-pc@Soumyabratas-MacBook-Pro.local>
2023-12-19 21:56:30 +0530

    Added header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,22 @@
+import { faUser } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div id="header">
+      <div id="title">
+        <div>
+          <FontAwesomeIcon icon={faUser} style={{ marginRight: "20px" }} />
+          {process.env.REACT_APP_NAME}
+        </div>
+      </div>
+
+      <div>
+        <button className="custom-button">About</button>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
