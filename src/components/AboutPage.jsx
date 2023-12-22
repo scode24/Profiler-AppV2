b9f54c97dc47f8d9f53a21d97ae6891ac45a662e
Soumyabrata Sarkar <smy-pc@Soumyabratas-MacBook-Pro.local>
2023-12-22 19:22:31 +0530

    Changed about content

--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -7,62 +7,17 @@ function AboutPage() {
         <span>About This Project</span>
         <div className="note">
           <p>
-            Presenting <strong>"Profiler AppV2"</strong> – Your Gateway to a
-            Dynamic Professional Showcase! 🌟
-          </p>
-
-          <p>
-            <em>Profiler AppV2</em> has revolutionized the way you share your
-            professional journey. Seamlessly submit and showcase your skills,
-            projects, and more with an interactive twist!
-          </p>
-          <br></br>
-          <p>
-            <strong>Unleash the Power of Profiler AppV2:</strong>
-          </p>
-          <br></br>
-
-          <p>
-            <strong>Dynamic Profile Tiles:</strong> Elevate your profile with
-            captivating tiles. Experience a delightful animation that gracefully
-            expands each tile, unveiling additional details in an eye-catching
-            format.
-          </p>
-          <br></br>
-
-          <p>
-            <strong>User-Friendly Interface:</strong> Effortlessly tailor your
-            profile by opening and closing tiles. Your profile, your control –{" "}
-            <em>Profiler AppV2</em> ensures a personalized presentation every
-            time.
-          </p>
-          <br></br>
-
-          <p>
-            <strong>Customization at Your Fingertips:</strong> Stand out by
-            personalizing your tiles. Update JSON configuration values to tweak
-            styles, colors, and animation states. Craft a profile that reflects
-            your unique style.
-          </p>
-          <br></br>
-
-          <p>
-            <strong>HTML Formatting Support:</strong> Go beyond text with HTML
-            formatting in the JSON data. Showcase interactive content like
-            project demos and charts, making your profile truly stand out.
-          </p>
-          <br></br>
-
-          <p>
-            <strong>Mobile-Friendly Brilliance:</strong> Experience a seamless,
-            responsive design on the go. <em>Profiler AppV2</em> ensures your
-            profile shines on mobile devices.
-          </p>
-          <br></br>
-
-          <p>
-            Your professional story deserves the spotlight – with{" "}
-            <em>Profiler AppV2</em>, let your profile shine like never before!
+            Introducing Profile AppV2, an innovative application that allows
+            users to seamlessly submit their relevant profile information,
+            including skills and projects. The app features interactive tiles in
+            a sleek title format. Clicking on a tile triggers a smooth
+            animation, expanding to reveal more details. Users have the
+            flexibility to open and close tiles at will, ensuring a customizable
+            and user-friendly experience. The mobile-friendly UI enhances
+            accessibility, while advanced users can personalize tile styles and
+            animations through JSON config values, even incorporating HTML
+            formatting in the data. Elevate your profile with Profile AppV2 –
+            where customization meets simplicity.
           </p>
 
           <br></br>
