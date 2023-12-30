d41c34e5d63d1d5c2bd41613cf78fc5847d87373
Soumyabrata Sarkar <smy-pc@Soumyabratas-MacBook-Pro.local>
2023-12-30 18:20:15 +0530

    add cors origin

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,11 @@ const port = process.env.PORT;
 
 app.use(express.json({ limit: "25mb" }));
 app.use(express.urlencoded({ limit: "25mb" }));
-app.use(cors());
+app.use(
+  cors({
+    origin: "localhost:80",
+  })
+);
 
 app.use(userRouter);
 app.use(profileRouter);
